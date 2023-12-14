78a1e62a1f2cdf0f86a32544678da7353b039acf
BOlafB <olafmateusz72@gmail.com>
2023-12-14 08:10:18 +0100

    s

--- log-in.php
+++ log-in.php
@@ -1,10 +1,3 @@
-<?php
-require("db/users/findUserByLogin.php");
-require("db/users/findUserByUserId.php");
-require_once("db/connectDB.php");
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -12,13 +5,8 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-
-<style>
-
- 
-
-* {
+    <style>
+        * {
             font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
             box-sizing: border-box;
             margin: 0;
@@ -47,7 +35,7 @@ session_start();
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
             width: 300px;
             text-align: center;
-            color: #000;
+            color: #333;
             margin-bottom: 20px;
         }
 
@@ -95,40 +83,17 @@ session_start();
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
             width: 300px;
             text-align: center;
-            color: #000;
+            color: #333;
         }
-
-
-
-  
- </style>
-
-<?php
-
-if (isset($_POST['log-out'], $_SESSION['userId'])) {
-    $_SESSION['userId'] = null;
-    echo "wylogowano <a href>Logowanie</a>";
-    exit();
-}
-
-if (isset($_POST["password"], $_POST["login"])) {
-    $login = mysqli_real_escape_string(DB, trim($_POST["login"]));
-    $password = mysqli_real_escape_string(DB, trim($_POST['password']));
-
-    $foundUser = findUserByLogin($login);
-
-    if (password_verify($password, $foundUser['password'])) {
-        $_SESSION['userId'] = $foundUser['id_konta'];
-    }
-}
-?>
+    </style>
+</head>
 
 <body>
     <?php
     $showLogInForm = true;
     if (isset($_SESSION['userId'])) {
         $foundUser = findUserByUserId($_SESSION['userId']);
-        echo "jestes zalogowany jako: " . $foundUser['login'];
+        echo "Jesteś zalogowany jako: " . $foundUser['login'];
         $showLogInForm = false;
     }
     ?>
@@ -139,11 +104,16 @@ if (isset($_POST["password"], $_POST["login"])) {
         <button type="submit">Zaloguj</button>
     </form>
 
-    <a href="./register.php" class="regi">Rejestracja</a> 
+    <a href="./register.php">Rejestracja</a>
 
     <form action="log-in.php" method="post" <?php if (!isset($_SESSION['userId'])) echo "hidden" ?>>
-        <button type="submit" name="log-out">Wyloguj sie</button>
+        <button type="submit" name="log-out">Wyloguj się</button>
     </form>
+
+    <div class="register-panel">
+        <p>Nie masz konta? Zarejestruj się</p>
+        <a href="./register.php">Zarejestruj się</a>
+    </div>
 </body>
 
 </html>
